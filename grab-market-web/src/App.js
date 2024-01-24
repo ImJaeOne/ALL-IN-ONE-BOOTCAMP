770784321ac7773b912df89521a0ebf9f17b0118
ImJaeOne <dlawi0108@naver.com>
2024-01-25 00:24:59 +0900

    Grab Market
    react로 웹 개발하기
    useState/useEffect 중요

--- grab-market-web/src/App.js
+++ grab-market-web/src/App.js
@@ -1,29 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import ChildComponent from './child.js';
-import TimerComponent from './timer.js';
+import MainPageComponent from './main';
 
 function App() {
-    const text = '안녕하세요 그랩입니다.';
-    const sayHello = function () {
-        return <h3>인프런 강의 너무 좋아</h3>;
-    };
-    const sayHello2 = function () {
-        alert('안녕하세요');
-    };
-    return (
-        <div>
-            <h1>안녕하세요!</h1>
-            <h2>{text}</h2>
-            {sayHello()}
-            <div onClick={sayHello2}>클릭해주세요</div>
-            <TimerComponent />
-            <ChildComponent name="그랩" age={27} />
-            <ChildComponent name="민수" age={25} />
-            <ChildComponent name="철수" age={30} />
-            <ChildComponent name="슬기" age={28} />
-        </div>
-    );
+    return <MainPageComponent />;
 }
 
 export default App;
