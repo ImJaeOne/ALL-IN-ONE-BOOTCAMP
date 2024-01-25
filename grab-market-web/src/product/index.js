c8027f6fc315f057821c8938fd44bf6be7fa6915
ImJaeOne <dlawi0108@naver.com>
2024-01-25 22:27:25 +0900

    Grab Market
    상품 상세 페이지 구현
    atnd 적용

--- grab-market-web/src/product/index.js
+++ grab-market-web/src/product/index.js
@@ -1,8 +1,42 @@
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './index.css';
 
 function ProductPage() {
     const { id } = useParams();
-    return <h1>상품 상세 페이지 {id} 상품</h1>;
+    const [product, setProduct] = useState(null);
+    useEffect(function () {
+        axios
+            .get(`https://09461fd1-2e72-4648-aeac-5e21a39244a8.mock.pstmn.io/products/${id}`)
+            .then(function (result) {
+                setProduct(result.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, []);
+
+    if (product === null) {
+        return <h1>상품 정보를 받고 있습니다...</h1>;
+    }
+    return (
+        <div>
+            <div id="image-box">
+                <img src={'/' + product.imageUrl} />
+            </div>
+            <div id="profile-box">
+                <img src="/images/icons/avatar.png" />
+                <span>{product.seller}</span>
+            </div>
+            <div id="contents-box">
+                <div id="name">{product.name}</div>
+                <div id="price">{product.price}원</div>
+                <div id="createdAt">2024/01/25</div>
+                <div id="description">{product.description}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ProductPage;
